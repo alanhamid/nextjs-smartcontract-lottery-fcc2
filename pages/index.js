1a8d333611fb9fd5555316f895d80cd943d3019c
Alan Hamid <alanhamid13@gmail.com>
2022-08-22 16:44:56 -0400

    still getting error

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ import styles from "../styles/Home.module.css"
 import Header from "../components/Header"
 import LotteryEntrance from "../components/LotteryEntrance"
 import { useMoralis } from "react-moralis"
-// import { web3uikit } from "web3uikit"
-
 const supportedChains = ["31337", "4"]
 
 export default function Home() {
